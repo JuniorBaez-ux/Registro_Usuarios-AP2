03d3b4120ee745d85f33092c1a938eb3eeaea2e3
Junior Baez <junior_baez@ucne.edu.do>
2022-05-07 16:15:51 -0400

    Implementando el MainActivity del proyecto

--- app/src/main/java/com/example/registro_usarios_ap2/MainActivity.kt
+++ app/src/main/java/com/example/registro_usarios_ap2/MainActivity.kt
@@ -9,12 +9,18 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
 import com.example.registro_usarios_ap2.databinding.ActivityMainBinding
+import com.example.registro_usarios_ap2.model.Cliente
+import com.example.registro_usarios_ap2.viewmodel.ClienteViewModel
+import com.google.android.material.textfield.TextInputEditText
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class   MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: ClienteViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,37 +28,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        binding.guardarButton.setOnClickListener {
+            viewModel.guardar(Cliente(
+                0,
+                binding.clienteEditText.text.toString(),
+                binding.balanceEditText.floatValue()
+            ))
+        }
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        viewModel.guardado.observe(this) {
+            if (it) {
+                Snackbar.make(binding.balanceEditText, "Se guardo con exito!", Snackbar.LENGTH_LONG).show()
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    fun TextInputEditText.floatValue() = text.toString().toFloatOrNull() ?: 0.0f
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
