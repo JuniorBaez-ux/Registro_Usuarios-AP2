0f534d750623f1c159c0cc82c41ca23a7d947327
Junior Baez <junior_baez@ucne.edu.do>
2022-06-06 10:39:27 -0400

    Implementando diseño y programacion del proyecto con Compose

--- app/src/main/java/com/example/registro_usarios_ap2/MainActivity.kt
+++ app/src/main/java/com/example/registro_usarios_ap2/MainActivity.kt
@@ -1,52 +1,32 @@
 package com.example.registro_usarios_ap2
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import androidx.activity.ComponentActivity
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.android.material.textfield.TextInputEditText
-import dagger.hilt.android.AndroidEntryPoint
-import android.text.style.BackgroundColorSpan
 import androidx.activity.compose.setContent
-import androidx.compose.animation.expandHorizontally
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.registro_usarios_ap2.theme.RegistroPersonaComposeTheme
-import com.example.registro_usarios_ap2.util.Screen
+import com.example.registro_usarios_ap2.ui.componentes.ocupacion.ConsultaOcupacionesScreen
+import com.example.registro_usarios_ap2.ui.componentes.ocupacion.RegistroOcupacionesScreen
+import com.example.peopleregistercompose.ui.theme.RegistroPersonaComposeTheme
+import com.example.registro_usarios_ap2.ui.componentes.cliente.ConsultaClientesScreen
+import com.example.registro_usarios_ap2.ui.componentes.cliente.RegistroClientesScreen
+import dagger.hilt.android.AndroidEntryPoint
 
 /*
 @AndroidEntryPoint
@@ -57,41 +37,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }*/
+
+var selectedOcupacion: String? = null
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp()
-        }
-    }
-}
-
-@Composable
-fun MyApp(){
-    RegistroPersonaComposeTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-            val navHostController = rememberNavController()
-            NavHost(navController = navHostController,
-                startDestination = Screen.ListadoPersonaScreen.route){
-
-                composable(route = Screen.ListadoPersonaScreen.route){
-                    ListadoPersonasScreen(goRegistroPersonas = {navHostController.navigate(Screen.RegistroPersonaScreen.route)},
-                        goListaOcupaciones = {navHostController.navigate(Screen.ListadoOcupacionesScreen.route)})
-                }
-
-                composable(route = Screen.RegistroPersonaScreen.route){
-                    RegistroPersonasScreen(backToListadoPersonas ={ navHostController.navigate(Screen.ListadoPersonaScreen.route)})
-                }
-
-                composable(route = Screen.ListadoOcupacionesScreen.route){
-                    ListaOcupacionScreen(goToRegistroOcupaciones = {navHostController.navigate(Screen.RegistroOcupacionesScreen.route)})
-                }
-
-                composable(route = Screen.RegistroOcupacionesScreen.route){
-                    registrosOcupacionScreen(backToListadoOcupaciones = {navHostController.navigate(Screen.ListadoOcupacionesScreen.route)})
+            RegistroPersonaComposeTheme() {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyApp()
                 }
             }
         }
@@ -99,6 +59,124 @@ fun MyApp(){
 }
 
 @Composable
+fun MyApp() {
+    val navHostController = rememberNavController()
+
+    NavHost(navController = navHostController, startDestination = "ConsultaClientes"){
+        composable("ConsultaOcupaciones"){
+            ConsultaOcupacionesScreen(navHostController = navHostController)
+        }
+        composable("RegistroOcupaciones"){
+            RegistroOcupacionesScreen(navHostController = navHostController)
+        }
+        composable("ConsultaClientes"){
+            ConsultaClientesScreen(navHostController = navHostController)
+        }
+        composable("RegistroClientes"){
+            RegistroClientesScreen(navHostController = navHostController)
+        }
+    }
+}
+
+/*@Composable
+fun ConsultaPersonaScreen(navHostController: NavHostController){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Consulta de Personas")}
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                navHostController.navigate("RegistroPersona")
+            }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+
+    ) {
+        Column(modifier = Modifier.padding(it).padding(8.dp)) {
+            Button(onClick = { navHostController.navigate("ConsultaOcupaciones") }) {
+                Text(text = "Nueva Ocupación")
+            }
+            val lista = listOf("Junior", "Stephany", "Darianna", "Goku")
+
+            LazyColumn(modifier = Modifier.fillMaxWidth()){
+                items(lista){
+                        nombre -> RowPersona(nombre = nombre)
+                }
+            }
+        }
+    }
+
+}*/
+
+/*@Composable
+fun RegistroPersonaScreen(navHostController: NavHostController){
+    var person by rememberSaveable(){
+        mutableStateOf("")
+    }
+    var email by rememberSaveable(){
+        mutableStateOf("")
+    }
+    var salario by rememberSaveable(){
+        mutableStateOf("")
+    }
+
+    val ocup = listOf("Maestro", "Ingeniero", "Doctor", "Carpinterio")
+    Column(modifier = Modifier.padding(8.dp)) {
+        OutlinedTextField(
+            value = person,
+            onValueChange = {person = it},
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Nombre de la Persona")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = null)
+            }
+        )
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Email")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = null)
+            }
+        )
+
+        OcupacionesSpinner(ocupacion = ocup)
+
+        OutlinedTextField(
+            value = salario,
+            onValueChange = {salario = it},
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Salario")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Check,
+                    contentDescription = null)
+            }
+        )
+
+        OutlinedButton(onClick = { navHostController.navigateUp() }) {
+            Text(text = "Guardar")
+        }
+    }
+}*/
+
+
+/*@Composable
 fun ListadoPersonasScreen(goRegistroPersonas:() -> Unit, goListaOcupaciones:() -> Unit){
 
     val ScaffoldState = rememberScaffoldState()
@@ -143,9 +221,48 @@ fun ListadoPersonasScreen(goRegistroPersonas:() -> Unit, goListaOcupaciones:() -
             }
         }
     }
-}
+}*/
 
 @Composable
+fun OcupacionesSpinner(ocupacion:List<String>){
+
+    var ocupacionText by remember{
+        mutableStateOf("")
+    }
+    var expended by remember{
+        mutableStateOf(false)
+    }
+    Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+        Row(Modifier
+            .fillMaxWidth()
+            .clickable {
+                expended = !expended
+            }
+            .padding(8.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+            Text(text = ocupacionText, fontSize = 18.sp, modifier = Modifier.padding(end = 8.dp))
+            Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "")
+            DropdownMenu(expanded = expended, onDismissRequest = { expended = false}) {
+                ocupacion.forEach {
+                        ocupacion -> DropdownMenuItem(onClick = {
+
+                    expended = false
+                    ocupacionText = ocupacion.toString()
+                    selectedOcupacion = ocupacion
+                }) {
+                    Text(text = ocupacion.toString())
+                }
+                }
+
+            }
+        }
+    }
+}
+
+/*@Composable
 fun RegistroPersonasScreen(backToListadoPersonas:() -> Unit){
 
     var nombres by rememberSaveable() {
@@ -280,6 +397,20 @@ fun registrosOcupacionScreen(backToListadoOcupaciones:() -> Unit){
                 Text(text = "Guardar")
             }
         }
+    }
+}*/
+
+@Composable
+fun RowPersona(nombre:String){
+    Row() {
+        Text(text = "El nombre es: $nombre")
+    }
+}
+
+@Composable
+fun RowOcupacion(ocupacion:String){
+    Row() {
+        Text(text = "El nombre de la Ocupación es: $ocupacion")
     }
 }
 
